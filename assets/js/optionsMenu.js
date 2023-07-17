3d23d30237b2585796443dc22309755b9df54918
RubinoD1 <Rubinod1@gmail.com>
2023-07-17 11:25:45 -0400

    cleaned up options.html

--- assets/js/optionsMenu.js
+++ assets/js/optionsMenu.js
@@ -1,1 +1,14 @@
+//DOM elements 
+const optionsButton = document.getElementById("options-button"); //options button (in-game settings) 
+ //options menu
 
+//hides homepage and unhides the settings menu 
+optionsButton.addEventListener("click", function(){
+    settings.classList.remove("hidden"); //unhide options by removing the hidden class 
+});
+
+
+
+
+//event.target || share event listener with two buttons 
+//if/else statement if settings hide hero / if options just unhide options menu 
